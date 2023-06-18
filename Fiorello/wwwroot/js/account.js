b201f12a3e45611a02df19ad0c1a0a90694ea1d5
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-06-18 08:00:38 +0400

    eye task is ready

---
+++ Fiorello/wwwroot/js/account.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $('#checkeye1').click(function () {
+        var previousType = $(this).prev().attr("type");
+
+        if (previousType === "password") {
+            $(this).prev().removeAttr("type").attr("type", "text");
+        } else {
+            $(this).prev().removeAttr("type").attr("type", "password");
+        }
+    });
+
+    $('#checkeye2').click(function () {
+        var previousType = $(this).prev().attr("type");
+
+        if (previousType === "password") {
+            $(this).prev().removeAttr("type").attr("type", "text");
+        } else {
+            $(this).prev().removeAttr("type").attr("type", "password");
+        }
+    });
+
+    $('#checkeye').click(function () {
+        var previousType = $(this).prev().attr("type");
+
+        if (previousType === "password") {
+            $(this).prev().removeAttr("type").attr("type", "text");
+        } else {
+            $(this).prev().removeAttr("type").attr("type", "password");
+        }
+    });
+});
